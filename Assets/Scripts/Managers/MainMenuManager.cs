41cb7603b0a15826b8e909bf045ee538686138fe
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2023-04-12 15:12:20 +0700

    feat: add volume slider and player name input

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -6,6 +6,21 @@ using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] Slider volumeSlider;
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            LoadVolumeSetting();
+        } 
+        else
+        {
+            LoadVolumeSetting();
+        }
+    }
+
     public void LoadFirstQuest(){
         // unload the current scene
         // SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
@@ -19,4 +34,25 @@ public class MainMenuManager : MonoBehaviour
         SceneManager.LoadSceneAsync("QuestMenu");
     }
 
+    public void QuitGame()
+    {
+        // quit the game
+        Application.Quit();
+    }
+
+    public void SetVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        SaveVolumeSetting();
+    }
+
+    private void LoadVolumeSetting()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void SaveVolumeSetting()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
 }
